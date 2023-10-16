be863c8375752d0270d46162387c5b77262ac235
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-16 07:55:02 -0700

    Fixed negative for handling int

--- handle_int.c
+++ handle_int.c
@@ -9,36 +9,43 @@
 int handle_int(va_list args)
 {
 int n = va_arg(args, int);
-int count = 0;
-int div = 1;
-int temp = n;
-int digit;
+int count = 0, div = 1;
+int temp = n, digit;
 
 	if (n < 0)
 	{
 		_printchar('-');
 		count++;
-		n = -n;
 	}
-
-	while (temp > 9)
+	if (n == 0)
 	{
-		temp /= 10;
-		div *= 10;
-	}
-
-	temp = n;
-
-	while (div > 0)
-	{
-
-		digit = (temp / div) + '0';
-
-		_printchar(digit);
+		_printchar('0');
 		count++;
-		temp %= div;
-		div /= 10;
 	}
+	else
+	{
+		while (temp > 9 || temp < -9)
+		{
+			temp /= 10;
+			div *= 10;
+		}
+		if (n < 0)
+		{
+			temp = -n;
+		}
+		else
+		{
+			temp = n;
+		}
+		while (div > 0)
+		{
 
+			digit = (temp / div) + '0';
+			_printchar(digit);
+			count++;
+			temp %= div;
+			div /= 10;
+		}
+	}
 	return (count);
 }
