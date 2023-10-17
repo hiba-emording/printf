e3f080490f97bb9a15de9568863203cfceb5eecc
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-17 14:13:30 -0700

    handle char trial to fix checks

--- handle_char.c
+++ handle_char.c
@@ -10,21 +10,7 @@ int handle_char(va_list args)
 {
 char c = va_arg(args, int);
 
-char null_char = '\0';
+_printchar(c);
 
-int count = 0;
-
-	if (c == '\0')
-	{
-		_printchar(null_char);
-		count++;
-	}
-
-	else
-	{
-		_printchar(c);
-		count++;
-	}
-
-	return (count);
+return (1);
 }
