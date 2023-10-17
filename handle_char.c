b92fb68d2cab80b72d8da70fc68af47eca1a9472
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-16 21:10:09 -0700

    Handling nil char and pointer

--- handle_char.c
+++ handle_char.c
@@ -3,14 +3,33 @@
 /**
  * handle_char - prints a char.
  * @args: A va_list containing the char to be printed.
- * Return: number of chars printed (1).
+ * Return: number of chars printed.
  */
 
 int handle_char(va_list args)
 {
-char c = va_arg(args, int);
+char *c = va_arg(args, char *);
 
-_printchar(c);
+char *nil_str = "(nil)";
 
-return (1);
+int count = 0;
+
+	if (c == NULL)
+	{
+		while (*nil_str)
+		{
+			_printchar(*nil_str);
+			count++;
+			nil_str++;
+		}
+
+	}
+
+	else
+	{
+		_printchar(*c);
+		count++;
+	}
+
+	return (count);
 }
--- handle_ptr.c
+++ handle_ptr.c
@@ -10,13 +10,26 @@
 int handle_pointer(va_list args)
 {
 void *ptr = va_arg(args, void*);
-
+char *nil_str = "(nil)";
 int count = 0;
+int i;
 
-	count += _printchar('0');
-	count += _printchar('x');
+	if (ptr == NULL)
+	{
+		for (i = 0; nil_str[i] != '\0'; i++)
+		{
+			_printchar(nil_str[i]);
+			count++;
+		}
+	}
 
-	count += print_number((unsigned long)ptr, 16, 0);
+	else
+	{
+		count += _printchar('0');
+		count += _printchar('x');
+
+		count += print_number((unsigned long)ptr, 16, 0);
+	}
 
 	return (count);
 }
